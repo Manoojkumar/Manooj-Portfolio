5cfed0ff8af952250dd57c54462fb1c68897c209
Manoojkumar <heartlymno005@gmail.com>
2022-07-29 22:27:22 +0530

    social data added, and updated

--- src/data/aboutData.js
+++ src/data/aboutData.js
@@ -1,7 +1,7 @@
 export const aboutData = {
     title: "Who I am",
     description1: "My name's Manooj. I'm a iOS  designer and developer based in Coimbatore, Tamilnadu, IN.",
-    description2: "3+ Years expirence developing iOS Applications, Completed over 10+ projects in iOS. Currently learning React JS for FrontEnd Development. Totally 4+ years in IT field. ",
+    description2: "3+ Years experience developing iOS Applications, Completed over 10+ projects in iOS. Currently learning React JS for FrontEnd Development. Totally 4+ years in IT field. ",
     description3: "Right Now Working as Senior System Engineer at Infosys.",
     
     image: 2
--- src/data/headerData.js
+++ src/data/headerData.js
@@ -3,7 +3,7 @@ import resume from '../assets/pdf/resume-manooj.pdf'
 export const headerData = {
     name: 'Manooj',
     title: "iOS Developer & Frontend Web Developer",
-    desciption:"High level expirence in developing iOS Applications. Currently Learning Web development. I love to learn and explore new arenas.",
+    desciption:"High level experience in developing iOS Applications. Currently Learning Web development. I love to learn and explore new arenas.",
     image: 'https://raw.githubusercontent.com/Manoojkumar/Fooddy/master/292A1273.jpg',
     resumePdf: resume
 }
